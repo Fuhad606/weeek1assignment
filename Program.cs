f235851c403617a497869a86788f7ef049db042e
BakareHalimat <halimatbakare10@gmail.com>
2022-04-22 11:17:17 +0100

    week1assignment

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace week1assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello my name is Fuhad , i'm in backend track of Wella schools");
+            Console.WriteLine("I am learning backend development because I think it is the most important part of programming.Also it has a vital role in Web application performance. ");
+            Console.WriteLine("I'm also  learning it to boost my professional skills to become a distinguished figure.");
+        }
+    }
+}
